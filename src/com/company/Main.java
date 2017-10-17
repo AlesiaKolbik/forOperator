6c19c0527b8188ce530589e1c5ffe57794d76786
alesya.kolbik <moyaalesia@mail.ru>
2017-10-17 15:39:33 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int f = 1;
+        for (int i = 1; i <= 10; i++)
+        {
+            f = f * i; }
+        {
+            System.out.println("f=" + f);
+        }
+    }
+}
